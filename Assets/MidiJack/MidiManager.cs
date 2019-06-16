949b1e8626fb4dd0d7b78f2a14c525fef34e4c73
segur <segur.opus@gmail.com>
2019-06-16 21:56:23 +0900

    Create a process to move the cube corresponding to the note number

--- Assets/MidiJack/MidiManager.cs
+++ Assets/MidiJack/MidiManager.cs
@@ -1,30 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace MidiJack
 {
     public class MidiManager : MonoBehaviour
     {
-        private int _KnobNum = 8;
+        // キューブ群
         public GameObject[] meter;
-        private Vector3[] _pos;
+
+        // 位置キャッシュ
+        private Vector3[] _posList;
+
+        // 8個のキューブに対応するノートナンバーを定義
+        private int[] _noteNumberList = { 48, 50, 52, 53, 55, 57, 59, 60 };
+
+        /// <summary>
+        /// 初期化処理
+        /// </summary>
         void Start()
         {
-            _pos = new Vector3[_KnobNum];
-            for (int i = 0; i < _KnobNum; i++){
-                _pos[i] = meter[i].transform.position;
-            }
-           
+            // キューブ群の位置座標をキャッシュ
+            _posList = meter.Select(cube => cube.transform.position).ToArray();
         }
 
+        /// <summary>
+        /// イベントループ 
+        /// </summary>
         void Update()
         {
-            for (int i = 0; i < _KnobNum; i++)
+            for (int i = 0; i < meter.Length; i++)
             {
+                // MIDIからノートナンバーに対応するKeyの出力を取得し、キューブのy座標に設定
+                _posList[i].y = MidiMaster.GetKey(MidiChannel.Ch1, _noteNumberList[i]);
 
-                meter[i].transform.position =
-                            new Vector3(_pos[i].x, MidiMaster.GetKnob(MidiChannel.Ch1, i)*5.0f, _pos[i].z);
+                // キューブ位置を更新
+                meter[i].transform.position = _posList[i];
             }
         }
     }
